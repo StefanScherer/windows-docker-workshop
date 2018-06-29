7aa741f920762ac89f5c0d8a08d5dd96bb27a1a5
Stefan Scherer <scherer_stefan@icloud.com>
2018-06-29 23:58:33 +0200

    Add some user input

--- prepare-vms/azure/packer/provision.ps1
+++ prepare-vms/azure/packer/provision.ps1
@@ -63,4 +63,4 @@ Write-Host Install all Windows Updates
 Get-Content C:\windows\system32\en-us\WUA_SearchDownloadInstall.vbs | ForEach-Object {
   $_ -replace 'confirm = msgbox.*$', 'confirm = vbNo'
 } | Out-File $env:TEMP\WUA_SearchDownloadInstall.vbs
-"a`na" | cscript $env:TEMP\WUA_SearchDownloadInstall.vbs
+"a`na`na`na" | cscript $env:TEMP\WUA_SearchDownloadInstall.vbs
