b676738a6343a407f5e793920d25a64f7d576a8a
Stefan Scherer <scherer_stefan@icloud.com>
2018-06-30 15:39:24 +0200

    Fix

--- prepare-vms/aws/main.tf
+++ prepare-vms/aws/main.tf
@@ -85,7 +85,7 @@ $ProgressPreference = 'SilentlyContinue'
 Write-Host Set Password
 netsh advfirewall firewall add rule name="WinRM in" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow
 $admin = [adsi]("WinNT://./administrator, user")
-$admin.psbase.invoke("SetPassword", "${var.admin_password}")
+$admin.psbase.invoke("SetPassword", "${var.admin_password[count.index]}")
 
 Write-Host Enable Remote Desktop
 set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
