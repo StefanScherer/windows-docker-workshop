fe66a08c2d0f7cd91fc2fc76bf152788d433bd9f
Stefan Scherer <scherer_stefan@icloud.com>
2018-10-04 13:58:58 +0200

    Update image names for 2019

--- prepare-vms/azure/terraform/provision.ps1
+++ prepare-vms/azure/terraform/provision.ps1
@@ -39,10 +39,9 @@ $shortcut.WorkingDirectory = "$Home"
 $Shortcut.Save()
 
 # Run some containers
-docker run --rm mcr.microsoft.com/windowsservercore-insider:10.0.17744.1001 cmd
-docker run --rm mcr.microsoft.com/nanoserver-insider:10.0.17744.1001 cmd
-docker run --rm mcr.microsoft.com/windows-insider:10.0.17744.1001 cmd
-
+docker run --rm mcr.microsoft.com/windows/servercore:ltsc2019 cmd
+docker run --rm mcr.microsoft.com/windows/nanoserver:1809 cmd
+# docker run --rm mcr.microsoft.com/windows-insider:10.0.17744.1001 cmd
 
 Write-Output Cleaning up
 Remove-Item C:\provision.ps1
