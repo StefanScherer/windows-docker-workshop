92e6032198ef90a3afd777b7a3e5b3b5720819ca
Stefan Scherer <stefan.scherer@docker.com>
2019-03-09 17:43:26 +0100

    Ignore reboot

--- prepare-vms/azure/packer/updates.ps1
+++ prepare-vms/azure/packer/updates.ps1
@@ -2,4 +2,4 @@ Install-Module -Name PSWindowsUpdate -RequiredVersion 2.1.1.2 -Force
 Get-Command -Module PSWindowsUpdate
 Write-Output Running Windows Updates
 Get-WUInstall -MicrosoftUpdate -AcceptAll 
-Get-WUInstall -Install -AcceptAll
+Get-WUInstall -Install -AcceptAll -IgnoreReboot
