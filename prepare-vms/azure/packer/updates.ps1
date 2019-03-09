fa923a2308f78c67ceeeccae2d346dd79f8cae3e
Stefan Scherer <stefan.scherer@docker.com>
2019-03-09 17:59:27 +0100

    Be more verbose

--- prepare-vms/azure/packer/updates.ps1
+++ prepare-vms/azure/packer/updates.ps1
@@ -1,5 +1,8 @@
 Install-Module -Name PSWindowsUpdate -RequiredVersion 2.1.1.2 -Force
 Get-Command -Module PSWindowsUpdate
-Write-Output Running Windows Updates
-Get-WUInstall -MicrosoftUpdate -AcceptAll 
+Write-Output Listing Windows Updates
+$ProgressPreference = 'SilentlyContinue'
+Get-WUInstall -MicrosoftUpdate -AcceptAll
+Write-Output Installing Windows Updates
 Get-WUInstall -Install -AcceptAll -IgnoreReboot
+Write-Output Done.
