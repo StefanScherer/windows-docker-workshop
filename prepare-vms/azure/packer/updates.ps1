bbb51c5aa960353efe3daadfdc7e9da22cd09cb9
Stefan Scherer <scherer_stefan@icloud.com>
2019-03-09 14:31:55 +0100

    Install all Windows Updates

--- prepare-vms/azure/packer/updates.ps1
+++ prepare-vms/azure/packer/updates.ps1
@@ -1,3 +1,5 @@
-Write-Output "Installing kb4482887"
-curl.exe -o kb4482887.msu http://download.windowsupdate.com/c/msdownload/update/software/updt/2019/02/windows10.0-kb4482887-x64_826158e9ebfcabe08b425bf2cb160cd5bc1401da.msu
-Start-Process wusa.exe -ArgumentList ("kb4482887.msu", '/quiet', '/norestart', "/log:c:\Wusa.log") -Wait
+Write-Host Install all Windows Updates
+Get-Content C:\windows\system32\en-us\WUA_SearchDownloadInstall.vbs | ForEach-Object {
+ $_ -replace 'confirm = msgbox.*$', 'confirm = vbNo'
+} | Out-File $env:TEMP\WUA_SearchDownloadInstall.vbs
+"a`na" | cscript $env:TEMP\WUA_SearchDownloadInstall.vbs
