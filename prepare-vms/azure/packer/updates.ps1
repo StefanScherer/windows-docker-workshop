62ca91bbfb5319b25a9771df4a511784ff124030
Stefan Scherer <stefan.scherer@docker.com>
2019-03-09 18:22:08 +0100

    Wait for TiWorker

--- prepare-vms/azure/packer/updates.ps1
+++ prepare-vms/azure/packer/updates.ps1
@@ -6,3 +6,19 @@ Get-WUInstall -MicrosoftUpdate -AcceptAll
 Write-Output Installing Windows Updates
 Get-WUInstall -Install -MicrosoftUpdate -AcceptAll -IgnoreReboot
 Write-Output Done.
+
+$procname="TiWorker"
+
+$finished = 0
+
+while ($finished -lt 3) {
+
+  Start-Sleep 30
+  Write-Output "Checking for $procname ($finished)"
+  $output = "$(get-process -erroraction silentlycontinue $procname)"
+  if ( $output -eq "") {
+    $finished = $finished + 1
+  } else {
+    $finished = 0
+  }
+}
