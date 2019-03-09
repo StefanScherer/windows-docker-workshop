862493dab1c1dc9926cdc302a381b0718dc19212
Stefan Scherer <stefan.scherer@docker.com>
2019-03-09 16:49:28 +0100

    Remove update server

--- prepare-vms/azure/packer/updates.ps1
+++ prepare-vms/azure/packer/updates.ps1
@@ -1,4 +1,4 @@
 Install-Module PSWindowsUpdate
 Get-Command -Module PSWindowsUpdate
-Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d
+Write-Output Running Windows Updates
 Get-WUInstall -MicrosoftUpdate -AcceptAll 
