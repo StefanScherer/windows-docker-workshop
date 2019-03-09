61fba3ec4c464713cdcafb7b84f9572be790d1d9
Stefan Scherer <stefan.scherer@docker.com>
2019-03-09 17:37:53 +0100

    Download and install updates

--- prepare-vms/azure/packer/updates.ps1
+++ prepare-vms/azure/packer/updates.ps1
@@ -1,4 +1,5 @@
-Install-Module PSWindowsUpdate -Force
+Install-Module -Name PSWindowsUpdate -RequiredVersion 2.1.1.2 -Force
 Get-Command -Module PSWindowsUpdate
 Write-Output Running Windows Updates
 Get-WUInstall -MicrosoftUpdate -AcceptAll 
+Get-WUInstall -Install -AcceptAll
