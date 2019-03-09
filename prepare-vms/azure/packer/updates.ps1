3fe7012eb516051a5ba71235f025bfb78a8330a7
Stefan Scherer <scherer_stefan@icloud.com>
2019-03-09 13:50:06 +0100

    Install KB4482887

--- prepare-vms/azure/packer/updates.ps1
+++ prepare-vms/azure/packer/updates.ps1
@@ -1,3 +1,3 @@
-Install-Module PSWindowsUpdate -force
-Import-Module PSWindowsUpdate
-Get-WUInstall -AcceptAll | Out-File C:\PSWindowsUpdate.log
+Write-Output "Installing kb4482887"
+curl.exe -o kb4482887.msu http://download.windowsupdate.com/c/msdownload/update/software/updt/2019/02/windows10.0-kb4482887-x64_826158e9ebfcabe08b425bf2cb160cd5bc1401da.msu
+Start-Process wusa.exe -ArgumentList ("kb4482887.msu", '/quiet', '/norestart', "/log:c:\Wusa.log") -Wait
