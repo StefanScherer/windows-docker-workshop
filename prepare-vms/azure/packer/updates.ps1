a36839f77901d1d45cdf16e402e496019cc7409f
Stefan Scherer <stefan.scherer@docker.com>
2019-03-09 16:13:43 +0100

    Fix dashes

--- prepare-vms/azure/packer/updates.ps1
+++ prepare-vms/azure/packer/updates.ps1
@@ -1,4 +1,4 @@
 Install-Module PSWindowsUpdate
-Get-Command –module PSWindowsUpdate
+Get-Command -Module PSWindowsUpdate
 Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d
-Get-WUInstall –MicrosoftUpdate –AcceptAll 
+Get-WUInstall -MicrosoftUpdate -AcceptAll 
